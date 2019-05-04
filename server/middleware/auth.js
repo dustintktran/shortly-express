371fb962b7bf9782922e28516dc13c1456d3ae6c
dustintktran <dustintktran@gmail.com>
2019-05-03 17:35:28 -0700

    hello

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,26 +4,50 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
   // console.log(Object.keys(req.cookies));
-  console.log(req)
-  // let userId = models.Sessions.get({hash:req.cookies.shortlyid});
+  // console.log(req)
+  let userId;
+  let username;
+
+
   if (JSON.stringify(req.cookies) === '{}') {
     models.Sessions.create()
-      .then((data) => models.Sessions.get({id:data.insertId}))
+      .then((data) => models.Sessions.get({ id: data.insertId }))
       .then(data => {
-        req.session = {hash: data.hash}
+        req.session = { hash: data.hash }
 
         res.cookies = {};
-        res.cookies['shortlyid'] = {value: data.hash};
+        res.cookies['shortlyid'] = { value: data.hash };
+        next()
       })
-      .then(() => next())
-  } else if (true) {
-    console.log(req)
-    req.session = {hash: req.cookies.shortlyid}
-    next();
   } else {
-    //get out user id from Sessions
-    //get our user by id from users
-    //set session.user.username to equal it
+    models.Sessions.get({ hash: req.cookies.shortlyid })
+      .then(data => {
+        if (data) {
+          userId = data.userId
+        }
+      })
+      .then(() => {
+        if (!userId) {
+          req.session = { hash: req.cookies.shortlyid }
+          next();
+        } else {
+          //get out user id from Sessions
+          //get our user by id from users
+          models.Users.get({ id: userId })
+            .then(data => username = data.username)
+            .then(() => {
+              req.session = {};
+              req.session.user = {};
+              req.session.user.username = username;
+              req.session.userId = userId;
+              next();
+            })
+          //set session.user.username to equal it
+
+        }
+      })
+
+    // console.log(req)
   }
 
   // next();
